67cace143171705a3d628ea6837307897ef192bf
mdaja04 <136987019+mdaja04@users.noreply.github.com>
2024-09-14 16:03:52 +0100

    frontend-header-component

---
+++ frontend/src/components/CreateRecipeButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CreateRecipeButton.css'
+const CreateRecipeButton = () => {
+    return (
+
+        <button className="create-recipe-button-container">
+                Create
+        </button>
+
+
+    );
+};
+
+export default CreateRecipeButton;
---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SearchBar.css'
+const SearchBar = () => {
+    return (
+
+            <input
+                type="search"
+                className="search-input"
+                placeholder="Search"
+            />
+
+
+    );
+};
+
+export default SearchBar;

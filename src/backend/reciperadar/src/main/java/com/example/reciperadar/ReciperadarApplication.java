88c50197a1cdfed7b01a6cdb0d8b0fe76a54b371
mdaja04 <136987019+mdaja04@users.noreply.github.com>
2024-09-15 20:26:32 +0100

    Added generated spring initialzr files

---
+++ src/backend/reciperadar/src/main/java/com/example/reciperadar/ReciperadarApplication.java
@@ -0,0 +1,13 @@
+package com.example.reciperadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciperadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciperadarApplication.class, args);
+	}
+
+}
---
+++ src/backend/reciperadar/src/test/java/com/example/reciperadar/ReciperadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reciperadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciperadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

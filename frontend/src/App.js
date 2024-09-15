43f78982b7a7f559aa9408fefdc68b34a38cf1a2
mdaja04 <136987019+mdaja04@users.noreply.github.com>
2024-09-15 12:59:20 +0100

    Frontend SignUp Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,12 @@ import CreateRecipe from "./pages/CreateRecipe.jsx";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header.jsx";
 import Home from './pages/Home.jsx'
-import SignIn from "./pages/SignIn.jsx";
-
+import SignUp from "./pages/SignUp.jsx";
 
 function App() {
 
   return (
-      <SignIn></SignIn>
+      <SignUp></SignUp>
 
   );
 }

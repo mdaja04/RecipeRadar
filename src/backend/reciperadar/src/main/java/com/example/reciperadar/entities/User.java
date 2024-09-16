7b01acf569c8a5364b2b7c210fff7c76f9734bb0
mdaja04 <136987019+mdaja04@users.noreply.github.com>
2024-09-16 18:33:01 +0100

    added User.java entity

---
+++ src/backend/reciperadar/src/main/java/com/example/reciperadar/entities/User.java
@@ -0,0 +1,126 @@
+package com.reciperadar.models;
+
+import java.util.List;
+import java.time.LocalDate;
+
+public class User {
+    private Long id;
+    private String username;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private LocalDate dob;
+    private Integer age;
+    private String profileImageUrl;
+    private Boolean publicProfile;
+
+    public User(String username, String name, String surname, String email, String password, LocalDate dob, Integer age, String profileImageUrl, Boolean publicProfile) {
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.dob = dob;
+        this.age = age;
+        this.profileImageUrl = profileImageUrl;
+        this.publicProfile = publicProfile;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Boolean getPublicProfile() {
+        return publicProfile;
+    }
+
+    public void setPublicProfile(Boolean publicProfile) {
+        this.publicProfile = publicProfile;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                ", profileImageUrl='" + profileImageUrl + '\'' +
+                ", publicProfile=" + publicProfile +
+                '}';
+    }
+}
+

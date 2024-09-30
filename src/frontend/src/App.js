28bded640086390cf3c2510efe34984ff9c915ac
mdaja04 <136987019+mdaja04@users.noreply.github.com>
2024-09-30 18:18:44 +0100

    Redirect to login page if no token detected in local storage

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,6 +5,7 @@ import FinalPageSignUp from './pages/FinalPageSignUp';
 import HomePage from './pages/Home';
 import Verify from "./pages/Verify";
 import SignIn from "./pages/SignIn";
+import ProtectedRoute from "./components/PrivateRoutes";
 
 const App = () => {
     return (
@@ -13,7 +14,8 @@ const App = () => {
                 <Route path="/signup" element={<SignUp />} />  {/* Corrected the path */}
                 <Route path="/signup-complete" element={<FinalPageSignUp />} />
                 <Route path="/signin" element={<SignIn/>}/>
-                <Route path="/home" element={<HomePage />} />
+                <Route path="/home" element={<ProtectedRoute><HomePage/></ProtectedRoute>}/>
+
                 <Route path="/verify" element={<Verify/>} /> {/* Verify route */}
                 <Route path="*" element={<div>Page Not Found</div>} />
             </Routes>

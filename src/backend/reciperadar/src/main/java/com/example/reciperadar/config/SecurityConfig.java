7712a19522157280b9ca5169b866b13e34f4d2c1
mdaja04 <136987019+mdaja04@users.noreply.github.com>
2024-10-17 13:41:20 +0100

    added allowed origin pattern

--- src/backend/reciperadar/src/main/java/com/example/reciperadar/config/SecurityConfig.java
+++ src/backend/reciperadar/src/main/java/com/example/reciperadar/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("https://*.vercel.app"));
         configuration.setAllowedOrigins(List.of("https://*.vercel.app", "https://recipe-radar-five.vercel.app", "https://reciperadar-e0s5.onrender.com", "http://localhost:8080", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
